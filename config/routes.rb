d4691dd8aaff323c36d4429a23752676a86b6210
Michael <russellyates2013@gmail.com>
2014-11-03 14:05:53 +1300

    invert roots

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Blog::Application.routes.draw do
     resources :comments
   end
   # You can have the root of your site routed with "root"
-  root 'articles#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
