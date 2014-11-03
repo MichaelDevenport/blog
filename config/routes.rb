14684f23d2bda58aa8a2bcc7ae109a29863c502c
Michael <russellyates2013@gmail.com>
2014-11-03 13:43:43 +1300

    change in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Blog::Application.routes.draw do
     resources :comments
   end
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
